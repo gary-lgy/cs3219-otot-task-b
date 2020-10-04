a12c6f6f544b14b1bf706aa1947c4f08fdda3123
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2020-10-05 04:10:31 +0800

    Add coverall

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CS3219 "OTOT" Task B - REST API + React SPA + CI/CI + AWS Lambda
 
+[![Build Status](https://travis-ci.com/gary-lgy/cs3219-otot-task-b.svg?branch=main)](https://travis-ci.com/gary-lgy/cs3219-otot-task-b)
+
+[![Coverage Status](https://coveralls.io/repos/github/gary-lgy/cs3219-otot-task-b/badge.svg?branch=main)](https://coveralls.io/github/gary-lgy/cs3219-otot-task-b?branch=main)
+
 TODO
--- src/init/config.ts
+++ src/init/config.ts
@@ -1,25 +1,9 @@
 import dotenv from 'dotenv';
 
-type Environment = 'production' | 'development' | 'test';
-const environment: Environment =
-  process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test'
-    ? process.env.NODE_ENV
-    : 'development';
+// Try to load the values from .env
+dotenv.config();
 
-// For development, load the values from .env
-if (environment === 'development') {
-  const { error } = dotenv.config();
-  if (error) {
-    throw new Error(`Failed to load environment variables: ${error}`);
-  } else {
-    console.debug('Loaded environment variables from .env');
-  }
-}
-
-// Put the values in a nicely structured object.
-// Exits the process if a required variable is not found.
-
-function getValueOrExit(key: string): string {
+function getValueFromEnv(key: string): string {
   const maybeValue = process.env[key];
   if (maybeValue === undefined) {
     throw new Error(`Missing required config variable '${key}'`);
@@ -27,27 +11,33 @@ function getValueOrExit(key: string): string {
   return maybeValue;
 }
 
+// Put the values in a nicely structured object.
+type Environment = 'production' | 'development' | 'test';
 interface Config {
   environment: Environment;
   port?: string;
   databaseUrl: string;
 }
 
+let environment: Environment;
+let port: string | undefined = undefined;
 let databaseUrl: string;
 
-switch (environment) {
+switch (process.env.NODE_ENV) {
   case 'production':
-    databaseUrl = getValueOrExit('PROD_DATABASE_URL');
+    environment = 'production';
+    databaseUrl = getValueFromEnv('PROD_DATABASE_URL');
     break;
   case 'test':
-    databaseUrl = getValueOrExit('TEST_DATABASE_URL');
+    environment = 'test';
+    databaseUrl = getValueFromEnv('TEST_DATABASE_URL');
     break;
   default:
-    databaseUrl = getValueOrExit('DEV_DATABASE_URL');
+    environment = 'development';
+    port = getValueFromEnv('PORT');
+    databaseUrl = getValueFromEnv('DEV_DATABASE_URL');
     break;
 }
-
-const port = environment === 'development' ? getValueOrExit('PORT') : undefined;
 
 const config: Config = {
   environment,
